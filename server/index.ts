2e3f8140311fadb6fee0592844c8eb09e951537d
KeithNZ <132999619+K31thNZ@users.noreply.github.com>
2026-03-20 13:09:28 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -84,7 +84,7 @@ app.use((req, res, next) => {
     const { setupVite } = await import("./vite");
     await setupVite(httpServer, app);
   }
-
+app.get("/ping", (_req, res) => res.send("OK"));
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
